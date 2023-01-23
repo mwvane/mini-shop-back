5254c2cf8cee7bad2e3dc59d498c52e64c5b643e
mwvane <bzishvili57@gmail.com>
2023-01-23 15:06:32 +0400

    Create cart model

---
+++ mini-shop-api/Models/Cart.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mini_shop_api.Models
+{
+    public class Cart
+    {
+        [Required]
+        public int UserId { get; set; }
+        [Required]
+        public int ItemId { get; set; }
+    }
+}
