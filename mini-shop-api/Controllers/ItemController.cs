eb575a1ed5b93e7aba12c7227467e746b3609778
mwvane <bzishvili57@gmail.com>
2023-01-23 14:27:03 +0400

    Initial commit

---
+++ mini-shop-api/Controllers/ItemController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mini_shop_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+    }
+}
