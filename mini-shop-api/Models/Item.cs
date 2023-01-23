713378d8d7b2734d783b954740d3b273aed1170d
mwvane <bzishvili57@gmail.com>
2023-01-23 14:53:00 +0400

    Create item model

---
+++ mini-shop-api/Models/Item.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace mini_shop_api.Models
+{
+    public class Item
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
