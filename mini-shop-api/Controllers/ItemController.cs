4d1f289cb0a33fa2a333515f55cac2ea61f68161
mwvane <bzishvili57@gmail.com>
2023-01-24 16:29:54 +0400

    Implement DeleteCartItem, UpdateCartItemQuantity and GetItemQuantity functionality

--- mini-shop-api/Controllers/ItemController.cs
+++ mini-shop-api/Controllers/ItemController.cs
@@ -35,5 +35,45 @@ namespace mini_shop_api.Controllers
             }
             return cartItems;
         }
+        [HttpGet("getItemQuantity")]
+        public int GetItemQuantity(int id)
+        {
+            return _context.Items.Where(item => item.Id == id).FirstOrDefault().Quantity;
+        }
+        [HttpPost("updateCartItemQuantity")]
+        public bool UpdateCartItemQuantity([FromBody] Dictionary<string, int> payload)
+        {
+            int cartId = payload["id"];
+            int quantity = payload["quantity"];
+            try
+            {
+                Cart c = _context.Cart.Where(cartItem => cartItem.Id == cartId).FirstOrDefault();
+                if (c != null)
+                {
+                    c.Quantitiy = quantity;
+                    _context.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        [HttpPost("deleteCartItem")]
+        public bool deleteCartItem([FromBody] Dictionary<string,int> payload)
+        {
+            int id = payload["id"];
+            Cart c = _context.Cart.Where(item => item.Id == id).FirstOrDefault();
+            if(c != null)
+            {
+                _context.Cart.Remove(c);
+                _context.SaveChanges();
+                return true;
+            }
+            return false;
+        }
     }
 }
