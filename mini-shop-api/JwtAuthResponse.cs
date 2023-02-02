e4a75a4744c6533a1f2073f3cba8e0fc4417aace
mwvane <bzishvili57@gmail.com>
2023-02-02 21:38:12 +0400

    create JwtRespnse model

---
+++ mini-shop-api/JwtAuthResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace mini_shop_api
+{
+    [Serializable]
+
+    public class JwtAuthResponse
+    {
+        public string Token { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Id { get; set; }
+        public int Expires_in { get; set; }
+        public string Role { get; set; }    
+    }
+}
