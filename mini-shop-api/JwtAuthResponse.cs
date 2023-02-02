e593964c0472a26289e602b7fd451d6323d231b8
mwvane <bzishvili57@gmail.com>
2023-02-03 02:14:02 +0400

    Update JwtAuthResponse model

--- mini-shop-api/JwtAuthResponse.cs
+++ mini-shop-api/JwtAuthResponse.cs
@@ -5,10 +5,6 @@
     public class JwtAuthResponse
     {
         public string Token { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public int Id { get; set; }
-        public int Expires_in { get; set; }
-        public string Role { get; set; }    
+  
     }
 }
