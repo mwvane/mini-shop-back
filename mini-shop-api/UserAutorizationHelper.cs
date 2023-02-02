2f5b990f41d23d0e35c18600d2c9f6b8763c56ba
mwvane <bzishvili57@gmail.com>
2023-02-03 02:21:15 +0400

    Create helper class for validating user permissions

---
+++ mini-shop-api/UserAutorizationHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mini_shop_api.Models;
+
+namespace mini_shop_api
+{
+    public static class UserAutorizationHelper
+    {
+        public static bool CanCreateItem(User user)
+        {
+            if(user == null) return false;
+            if(user.Role == "user")
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool CanUpdateItem(User user, Item item)
+        {
+            if (user == null) return false;
+            if (user.Role == "user")
+            {
+                return false;
+            }
+            if(user.Role == "seller" && item.CreatedBy != user.Id) 
+            { 
+                return false;
+            }
+            return true;
+        }
+        public static bool CanDeleteItem(User user, Item item)
+        {
+            return CanUpdateItem(user, item);
+        }
+
+    }
+}
